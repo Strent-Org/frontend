691888fc0687ae72027d54f3761b59b2a96c14f8
Victor Iberi <iberivictor7@gmail.com>
2025-08-10 22:22:58 +0100

    Revert "Feature/shortlet"

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,10 @@ import './index.css';
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home'
-import Shortlets from './pages/Shortlets'
 import Nav from './components/Nav';
 import NotFound from './pages/NotFound';
 import Footer from './components/Footer';
-import InnerShortlet from './pages/InnerShortlet';
+import Nav from './components/Nav';
 function App() {
 
 
@@ -26,8 +25,6 @@ function App() {
         section i.e home
         <Route path="*" element={<NotFound />} />
         <Route path="/" element={<Home />} />
-        <Route path="/shortlet" element={<Shortlets />} />
-        <Route path="/innershortlet/:id" element={<InnerShortlet />} />
       </Routes>
       {/* footer should be here */}
       <Footer />
