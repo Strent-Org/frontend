59dab1d158296125075f9e4304dbdcbf58ae165a
Victor Iberi <iberivictor7@gmail.com>
2025-08-10 22:49:05 +0100

    Revert "Revert "Feature/shortlet""
    This reverts commit 691888fc0687ae72027d54f3761b59b2a96c14f8.

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,11 @@ import './index.css';
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home'
+import Shortlets from './pages/Shortlets'
 import Nav from './components/Nav';
 import NotFound from './pages/NotFound';
 import Footer from './components/Footer';
-import Nav from './components/Nav';
+import InnerShortlet from './pages/InnerShortlet';
 function App() {
 
 
@@ -25,6 +26,8 @@ function App() {
         section i.e home
         <Route path="*" element={<NotFound />} />
         <Route path="/" element={<Home />} />
+        <Route path="/shortlet" element={<Shortlets />} />
+        <Route path="/innershortlet/:id" element={<InnerShortlet />} />
       </Routes>
       {/* footer should be here */}
       <Footer />
